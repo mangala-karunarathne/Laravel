2f92d12ae475c85330c4d00695d954b0b00eeb79
mangala-karunarathne <mangalaicc@gmail.com>
2022-08-13 13:26:39 -0700

    Daily Task App

---
+++ useful.java
@@ -0,0 +1,25 @@
+>>>>>>
+
+// To create laravel app having name "example-app"
+
+composer create-project laravel/laravel:8.0 example-app 
+
+>>>>>>
+
+// 
+
+	1. 	Composer require laravel/jetstream
+	2.	php artisan jetstream:install livewire
+	3.	npm install
+	4.	npm run dev
+	5.	Changes in the table 
+	6.	php artisan migrate
+	
+>>>>> 
+	
+// To start back the created Project
+
+	1. Start the Xamp and run 
+	2. get the cmd in project directory
+	3. php artisan serve
+	
