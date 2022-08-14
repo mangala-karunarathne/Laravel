aff0ebad3157cdbe5d280fae5d91c4c1ac355ce9
mangala-karunarathne <mangalaicc@gmail.com>
2022-08-14 00:00:12 -0700

    Daily Tasks View with Routes

---
+++ taskApp/taskApp/resources/views/tasks.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bootstrap Site</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <style>
+      .container{
+      position: absolute;
+      top: 1%;
+      left:20%;
+      width: 100%;
+      text-align: center;
+      font-size: 18px;
+      margin: auto;
+      width: 60%;
+      border: 3px solid #73AD21;
+      padding: 10px;
+    }
+  </style>
+</head>
+<body>
+    <div class="container">
+        <div class="text">
+            <h2> Daily Tasks</h2>
+        </div>
+    </div>
+</body>
+</html>
